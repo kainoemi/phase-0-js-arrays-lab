350de8554e262ac20d1a64631a2645c9558ca2dd
Karen <karen.hrndz28@yahoo.com>
2025-01-12 13:20:52 -0600

    test first lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -8,5 +8,14 @@ module.exports = {
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+  // products
 };
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct(array) {
+  return array[0];
+}
+console.log(logFirstProduct(products));
+// function addProduct();
+// function updateProductName();
+// function removeLastProduct();
